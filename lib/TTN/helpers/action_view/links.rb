174c77ada127952315b496f2cd803f3a9705e3b6
Jonathan Davies <jonnie@throughthenet.com>
2011-03-08 15:20:16 +0000

    added asset helper wrappers

--- lib/TTN/helpers/action_view/links.rb
+++ lib/TTN/helpers/action_view/links.rb
@@ -7,6 +7,13 @@ module TTN
         link_to text, url, html
       end
       
+      def stylesheet(*args)
+        content_for :stylesheets, stylesheet_link_tag(*args)
+      end
+      
+      def javascript(*args)
+        content_for :javascripts, javascript_include_tag(*args)
+      end
     end
   end
 end
--- spec/integration/action_view_spec.rb
+++ spec/integration/action_view_spec.rb
@@ -5,4 +5,12 @@ describe ActionView::Base do
   it "has the link_to_with_current method" do
     ActionView::Base.new.should respond_to(:link_to_with_current)
   end
+  
+  it "has the stylesheet method" do
+    ActionView::Base.new.should respond_to(:stylesheet)
+  end
+  
+  it "has the javascript method" do
+    ActionView::Base.new.should respond_to(:javascript)
+  end
 end
